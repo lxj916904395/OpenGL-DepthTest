92b1920b2c39ffe68813733a3bff2206b1543478
lxj <lxj@lxjdeAir.lan>
2018-12-01 20:58:30 +0800

    first commit

--- OpenGL-正背面剔除/main.cpp
+++ OpenGL-正背面剔除/main.cpp
@@ -1,15 +1,9 @@
 //
-//  ViewController.h
+//  main.cpp
 //  OpenGL-正背面剔除
 //
 //  Created by lxj on 2018/12/1.
 //  Copyright © 2018 lxj. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
-
-@interface ViewController : NSViewController
-
-
-@end
-
+#include <stdio.h>
